e0c61d40170ac25a6067e8ae331e2e111bc1a4aa
Thermiee <navyblack16@example.com>
2022-04-13 23:43:39 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Navbar from './navbar/Navbar';
+import Countries from './components/countries/Countries';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello World</h1>
+      <Countries />
     </div>
   );
 }
---
+++ src/components/countries/Countries.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Countries = () => (
+  <div>Countries</div>
+);
+
+export default Countries;
