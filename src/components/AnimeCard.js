fd369f444eeb5e6f7e9533d0fc7ce37575d30e86
Thermiee <navyblack16@example.com>
2022-04-15 15:22:47 +0100

    create card components and pages

---
+++ src/components/AnimeCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AnimeCard = () => (
+  <div className="anime-card">
+    <div className="anime-card-image">
+      <img src="https://i.pinimg.com/originals/f4/b1/b9/f4b1b9f8f8f8f8f8f8f8f8f8f8f8f8f8.jpg" alt="anime" />
+    </div>
+    <div className="anime-card-info">
+      <h1>Anime Title</h1>
+      <p>Anime Description</p>
+    </div>
+  </div>
+);
+
+export default AnimeCard;
