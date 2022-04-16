0e51748808f85d150e780c29dfc97b7ebff831ea
Thermiee <navyblack16@example.com>
2022-04-16 23:56:19 +0100

    fix linters

--- src/App.test.js
+++ src/App.test.js
@@ -4,16 +4,15 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-const renderWithRedux = (ui, { store } = {}) => {
-  return {
-    ...render(<Provider store={store}>{ui}</Provider>),
-    store,
-  };
-} 
+const renderWithRedux = (ui, { store } = {}) => ({
+  ...render(<Provider store={store}>{ui}</Provider>),
+  store,
+});
 
 describe('App', () => {
   it('renders without crashing', () => {
     const { getByText } = renderWithRedux(<App />, { store });
+    const linkElement = getByText(/2022 AnimeWorld/i);
+    expect(linkElement).toBeInTheDocument();
   });
-}
-);
+});
