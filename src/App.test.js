3618b0c4b85138d5c91a4480d097f37f6db3341b
Thermiee <navyblack16@example.com>
2022-04-17 03:15:47 +0100

    Add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import animeReducer from './redux/anime/anime';
 
-const renderWithRedux = (ui, { store } = {}) => ({
-  ...render(<Provider store={store}>{ui}</Provider>),
-  store,
+it('Render HomePage', () => {
+  const page = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  ).toJSON();
+  expect(page).toMatchSnapshot();
 });
 
-describe('App', () => {
-  it('renders without crashing', () => {
-    const { getByText } = renderWithRedux(<App />, { store });
-    const linkElement = getByText(/2022 AnimeWorld/i);
-    expect(linkElement).toBeInTheDocument();
+it('Render fact page', () => {
+  const page = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  ).toJSON();
+  expect(page).toMatchSnapshot();
+});
+
+it('Fetch anime facts', () => {
+  const state = animeReducer(undefined, { type: 'FETCH_ANIME_FACTS' });
+  expect(state).toEqual({
+    animes: [],
   });
-});
+}
+);
+
+it ('Fetch anime', () => {
+  const state = animeReducer(undefined, { type: 'FETCH_ANIMES' });
+  expect(state).toEqual({
+    animes: [],
+  });
+}
+);
+
