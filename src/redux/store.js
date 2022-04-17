38d4a5d8c33e82c085740f4a437e8e49de465891
Thermiee <navyblack16@example.com>
2022-04-17 01:57:30 +0100

    initial deployment

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,12 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import animeReducer from './anime/anime';
 import fetchAnimeThunk from './animeThunk';
-// import fetchFactThunk from './factThunk';
-// import factReducer from './fact/fact';
 
 const rootReducer = combineReducers({
   animeReducer,
-  // factReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 store.dispatch(fetchAnimeThunk());
-// store.dispatch(fetchFactThunk());
 
 export default store;
